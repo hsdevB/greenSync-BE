b5020e4cd5d5cb4718c82060f241d56fac0f67a8
김병곤 <gon00130@gmail.com>
2025-07-08 12:37:04 +0900

    feat : 데이터 모델링 작업 진행

---
+++ greenSync/models/weather.js
@@ -0,0 +1,53 @@
+import {Model, DataTypes} from "sequelize";
+
+class Weather extends Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                isDay: {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false,
+                    comment: '밤낮여부'
+                },
+                fanIsRain: {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false,
+                    comment: '강수여부'
+                },
+                windSpeed: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false,
+                    comment: '풍속'
+                },
+                windDirection: {
+                    type: DataTypes.STRING(20),
+                    allowNull: false,
+                    comment: '풍향'
+                },
+                outsideTemp: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false,
+                    comment: '외부온도'
+                },
+                insolation: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false,
+                    comment: '일사량'
+                },
+            },
+            {
+                sequelize,
+                tableName: 'Weather',
+                timestamps: true,
+            }
+        );
+    }
+    static associate(db) {
+        db.Weather.belongsTo(db.Farm, { 
+            foreignKey: 'farmId',  
+            targetKey: 'farmCode',
+            as: 'farm' 
+        });
+    }
+};
+export default Weather;
