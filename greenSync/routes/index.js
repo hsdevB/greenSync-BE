3a4b4edf6e096c8c1563c76036890869d39e796a
김병곤 <gon00130@gmail.com>
2025-07-25 16:18:52 +0900

    fix : 풍향,풍속 실시간데이터 수집 수정 및 일사량 고정값받아오는 부분 수정

--- greenSync/routes/index.js
+++ greenSync/routes/index.js
@@ -7,6 +7,7 @@ import signupRouter from './signup.js';
 import weatherRouter from './weather.js';
 import sensorRouter from './sensor.js';
 import userRouter from './user.js';
+import predictRouter from './predict.js';
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.use('/signup', signupRouter);
 router.use('/weather', weatherRouter);
 router.use('/sensor', sensorRouter);
 router.use('/user', userRouter);
+router.use('/predict', predictRouter);
 
 export default router;
